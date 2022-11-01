b7ee9462f7151fe41d0e63f2b3f01e1b10122cf5
qurbonov <qurbonov.p@gmail.com>
2022-11-01 16:03:19 +0500

    getindividualinfo

--- src/components/cabinet/PersonalC.js
+++ src/components/cabinet/PersonalC.js
@@ -3,34 +3,33 @@ import axios from "axios";
 
 const PersonalC = () => {
     const [pinfl, setPinfl] = useState();
+    const [iResult, setIResult] = useState();
     const onChangePinfl = (e) => {
         const pinfl = e.target.value;
         setPinfl(pinfl);
     };
-    //
-    // const getIndividualInfo = async () => {
-    //     try {
-    //         const response = await axios.get(
-    //             process.env.DSQ_GET_INDIVIDUAL_BY_PINFL
-    //             // +`?pinfl=${pinfl}`
-    //         );
-    //         console.log(response)
-    //         // setPinfl(response.data.body);
-    //     } catch (error) {
-    //         console.log(error);
-    //     }
-    // };
     const headers = {
         'Authorization': 'Bearer ' + localStorage.getItem("token")
     };
-    useEffect(() => {
-        console.log(localStorage.getItem("token"))
-        // process.env.DSQ_GET_INDIVIDUAL_BY_PINFL
-        axios.get(`http://localhost:8585/v1/atm/dsq/getIndividualInfo?pinfl=51911046450026`, {headers})
-            .then(response => setPinfl(response.data))
-        console.log(pinfl)
-        // getIndividualInfo();
-    }, []);
+
+    const getIndividualInfo = async () => {
+        try {
+            const response = await axios.get(process.env.REACT_APP_LOCAL_URL_GET_INDIVIDUAL_BY_PINFL + `?pinfl=${pinfl}`, {headers});
+            console.log(response)
+            setIResult(response.data.body);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    // useEffect(() => {
+    //     console.log(localStorage.getItem("token"))
+    //     // process.env.DSQ_GET_INDIVIDUAL_BY_PINFL
+    //     axios.get(`http://localhost:8585/v1/atm/dsq/getIndividualInfo?pinfl=51911046450026`, {headers})
+    //         .then(response => setPinfl(response.data))
+    //     console.log(pinfl)
+    //     // getIndividualInfo();
+    // }, []);
 
 
     return (
@@ -40,7 +39,7 @@ const PersonalC = () => {
             </h1>
             <p>YTT PINFLni kiriting:</p>
             <input type="text" name="pinfl" id="" className="form-text" onChange={onChangePinfl}/>
-            <button className="btn btn-light"> qidirish</button>
+            <button className="btn btn-light" onClick={getIndividualInfo}> qidirish</button>
         </div>
     );
 };
