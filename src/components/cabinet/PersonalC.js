1bd2d235fec18a8c48a22f80ac26777ac0e8bf0c
qurbonov <qurbonov.p@gmail.com>
2022-11-02 17:13:39 +0500

    getYuridik

--- src/components/cabinet/PersonalC.js
+++ src/components/cabinet/PersonalC.js
@@ -4,18 +4,18 @@ import {FcSearch} from 'react-icons/fc';
 
 const PersonalC = () => {
 
+    const headers = {
+        'Authorization': 'Bearer ' + localStorage.getItem("token")
+    };
+
+    /* Yakka tartibdagi tadbirkor */
     const [pinfl, setPinfl] = useState();
+    const [dataIn, setData] = useState();
 
     const onChangePinfl = (e) => {
         const pinfl = e.target.value;
         setPinfl(pinfl);
     };
-
-    const headers = {
-        'Authorization': 'Bearer ' + localStorage.getItem("token")
-    };
-    const [dataIn, setData] = useState();
-
     const getIndividualInfo = () => {
         axios.get(process.env.REACT_APP_LOCAL_URL_GET_INDIVIDUAL_BY_PINFL + `?pinfl=${pinfl}`, {headers})
             .then((response) => {
@@ -61,13 +61,69 @@ const PersonalC = () => {
                 </table>
             </>
 
-        ) : <div className="container">Ma'lumot topilmadi !!</div>
+        ) : <div className="container"></div>
     }
+
+    /* Yuridik shaxs */
+
+    const [stir, setStir] = useState();
+    const [YuridikData, setYuridikData] = useState();
+
+    const onChangeStir = (e) => {
+        const stir = e.target.value;
+        setPinfl(stir);
+    };
+
+    const getYuridikInfo = () => {
+        axios.get(process.env.REACT_APP_LOCAL_URL_GET_GET_YURIDIK_BY_STIR + `?stir=${stir}`, {headers})
+            .then((response) => {
+                setYuridikData(response.data);
+            }).catch(
+            (error) => {
+                console.log(error);
+            });
+    }
+
+    const dispYuridikData = () => {
+        return YuridikData ? (
+            <>
+                <table className="table table-hover table-striped">
+                    <thead>
+                    <th>F.I.O (nomi</th>
+                    <th>{YuridikData.body.company.name}</th>
+                    </thead>
+                    <tr>
+                        <td>Pinfl</td>
+                        <td>{YuridikData.body.company.shortName}</td>
+                    </tr>
+                    <tr>
+                        <td>STIR raqami</td>
+                        <td>{YuridikData.body.company.tin ? YuridikData.body.company.tin : "-"}</td>
+                    </tr>
+                    <tr>
+                        <td>Ro'yhatga olingan sana</td>
+                        <td>{YuridikData.body.company.registrationDate}</td>
+                    </tr>
+                    <tr>
+                        <td>Ro'yhatga olish raqam</td>
+                        <td>{YuridikData.body.company.registrationNumber}</td>
+                    </tr>
+                    <tr>
+                        <td>Rahbar</td>
+                        <td>{YuridikData.body.director.lastName} {YuridikData.body.director.firstName} {YuridikData.body.director.middleName}</td>
+                    </tr>
+                    
+                </table>
+            </>
+
+        ) : <div className="container"></div>
+    }
+
     return (
         <>
             <div className="container mt-3 shadow-sm p-4 rounded">
                 <h5>
-                    Yuridik shaxslar va YTT lar to'g'risida ma'lumot
+                    Yakka tartibdagi tadbirkor (YTT) to'g'risida ma'lumot
                 </h5>
                 <div className="bg-light border p-3 rounded-3">
                     PINFL:
@@ -79,6 +135,23 @@ const PersonalC = () => {
             <div className="container mt-3">
                 {dispData()}
             </div>
+
+
+            <div className="container mt-3 shadow-sm p-4 rounded">
+                <h5>
+                    Yuridik shaxs to'g'risida ma'lumot
+                </h5>
+                <div className="bg-light border p-3 rounded-3">
+                    STIR:
+                    <input type="text" name="stir" id="" className="form-text ms-2" onChange={onChangeStir}/>
+                    <button className="btn btn-sm btn-outline-secondary ms-2" onClick={getYuridikInfo}><FcSearch/>
+                    </button>
+                </div>
+            </div>
+            <div className="container mt-3">
+                {dispYuridikData()}
+            </div>
+
         </>
     );
 };
