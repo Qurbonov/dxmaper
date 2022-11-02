4ce25bb0d78760a65c20e81f99106e42949b7ffb
qurbonov <qurbonov.p@gmail.com>
2022-11-02 12:24:11 +0500

    getindividualinfov2

--- src/components/cabinet/PersonalC.js
+++ src/components/cabinet/PersonalC.js
@@ -2,46 +2,124 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 
 const PersonalC = () => {
+
     const [pinfl, setPinfl] = useState();
-    const [iResult, setIResult] = useState();
+
     const onChangePinfl = (e) => {
         const pinfl = e.target.value;
         setPinfl(pinfl);
     };
+
     const headers = {
         'Authorization': 'Bearer ' + localStorage.getItem("token")
     };
+    const [dataIn, setData] = useState();
 
-    const getIndividualInfo = async () => {
-        try {
-            const response = await axios.get(process.env.REACT_APP_LOCAL_URL_GET_INDIVIDUAL_BY_PINFL + `?pinfl=${pinfl}`, {headers});
-            console.log(response)
-            setIResult(response.data.body);
-        } catch (error) {
-            console.log(error);
-        }
-    };
-
-    // useEffect(() => {
-    //     console.log(localStorage.getItem("token"))
-    //     // process.env.DSQ_GET_INDIVIDUAL_BY_PINFL
-    //     axios.get(`http://localhost:8585/v1/atm/dsq/getIndividualInfo?pinfl=51911046450026`, {headers})
-    //         .then(response => setPinfl(response.data))
-    //     console.log(pinfl)
-    //     // getIndividualInfo();
-    // }, []);
+    const getIndividualInfo = () => {
+        axios.get(process.env.REACT_APP_LOCAL_URL_GET_INDIVIDUAL_BY_PINFL + `?pinfl=${pinfl}`, {headers})
+            .then((response) => {
+                setData(response.data);
+                console.log(response.data);
 
 
-    return (
-        <div className="container mt-3">
-            <h1>
-                Yuridik shaxslar va YTT lar to'g'risida ma'lumot
-            </h1>
-            <p>YTT PINFLni kiriting:</p>
-            <input type="text" name="pinfl" id="" className="form-text" onChange={onChangePinfl}/>
-            <button className="btn btn-light" onClick={getIndividualInfo}> qidirish</button>
-        </div>
-    );
-};
+                console.log(dataIn);
+            }).catch(
+            (error) =>{
+                console.log(error);
+            });
+    }
+    //
+    // {
+    //     "body": {
+    //     "pinfl": 51911046450026,
+    //         "tin": null,
+    //         "name": "ABDULHAMIDOV YUNUS ABDULBOQI O‘G‘LI",
+    //         "isEntrepreneur": 1,
+    //         "registrationDate": "05.07.2022",
+    //         "registrationId": 1338737,
+    //         "activityTypeName": "Компьютер дастурларини ишлаб чикиш хизматлари, компьютер ўйинларини ташкил этиш, шунингдек компьютер ёрдамида матнларни териш ва босиб чикариш хизматлари, матндан нусха олишва кўпайтириш билан боглик хизматлар",
+    //         "status": 1,
+    //         "liquidationDate": null,
+    //         "suspensionDate": null,
+    //         "activityTypeId": "277",
+    //         "vatNumber": null,
+    //         "beginDate": "05.07.2022",
+    //         "endDate": "31.07.2025",
+    //         "entrepreneurshipAddress": {
+    //         "soatoCode": "1724401",
+    //             "regionId": 24,
+    //             "districtId": 10,
+    //             "address": "SIRDARYO VILOYATI, GULISTON SHAHRI"
+    //     }
+    // },
+    //     "success": true
+    // }
+    const dispData=()=>{
+        return dataIn ? (
+            <table>
+                <thead>
+                <th>F.I.O. (nomi)</th>
+                <th>dataIn.body.name</th>
+                </thead>
+                <tr>
+                    <td>dataIn.body.</td>
+                    <td>dataIn.body.</td>
+                </tr>
+                <tr>
+                    <td>dataIn.body.</td>
+                    <td>dataIn.body.</td>
+                </tr>
+                <tr>
+                    <td>dataIn.body.</td>
+                    <td>dataIn.body.</td>
+                </tr>
+                <tr>
+                    <td>dataIn.body.</td>
+                    <td>dataIn.body.</td>
+                </tr>
+                <tr>
+                    <td>dataIn.body.</td>
+                    <td>dataIn.body.</td>
+                </tr>
+                <tr>
+                    <td>dataIn.body.</td>
+                    <td>dataIn.body.</td>
+                </tr>
+                <tr>
+                    <td>dataIn.body.</td>
+                    <td>dataIn.body.</td>
+                </tr>
+                <tr>
+                    <td>dataIn.body.</td>
+                    <td>dataIn.body.</td>
+                </tr>
+            </table>
 
-export default PersonalC;
+            <b>dataIn.body.pinfl<b> <br/>
+
+
+                ): <h2>no data</h2>
+                }
+                return (
+                <>
+                    {dispData()}
+                    <div className="container mt-3">
+                        <h1>
+                            Yuridik shaxslar va YTT lar togrisida malumot
+                        </h1>
+
+                        <p>YTT PINFLni kiriting:</p>
+                        <input type="text" name="pinfl" id="" className="form-text" onChange={onChangePinfl}/>
+                        <button className="btn btn-light" onClick={getIndividualInfo}> qidirish</button>
+                    </div>
+                    <div>
+                        pinfl
+
+                    </div>
+                </>
+                );
+                };
+
+
+
+                export default PersonalC;
